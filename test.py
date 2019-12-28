6ddcb51832bdf9e666036906e24a49a2033088f0
katharact <katharact@gmail.com>
2019-12-29 00:26:22 +0100

    remove print

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 def main():
-    print("Hello")
     print("Va chier")
-    print("MEEEEEEEEERRRRRRRDDDDDEEEEEEEE")
-    print("HALLELUHYAH!")
-    print("HAHAHAHAHAHAHHAHAHA")
 
 
 if __name__ == '__main__':
