1a4208b4f9ccfb42ab5d75acaa5f2d049c27f0b5
katharact <katharact@gmail.com>
2019-12-29 01:50:13 +0100

    add more lines

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 def main():
     print("Va chier")
     print("la mort")
+    print("la mort")
+    print("la mort")
+    print("la mort")
+    print("la mort")
+    print("la mort")
 
+    
 if __name__ == '__main__':
     main()
